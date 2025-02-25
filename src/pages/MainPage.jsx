b5251dfce50988f2d06d23f8e5568e1b79ef7646
Miel-cyb <oseinovelta@gmail.com>
2025-02-25 09:57:09 +0000

    updated

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -20,14 +20,14 @@ function MainPage(){
                 <p className="md:text-[17px]">Our distribution company thrives on strong relationships with its customers,partners, and drivers, ensuring smooth operations and long-term success. <br />Here’s how these connections are maintained and enhanced</p>
                 <hr className="bg-lime-700 h-1 w-44 mt-3 mx-auto" />
             </div>
-            <Layout image="/customer.jpg" title="Customer" desc="Customers are the backbone of any distribution company. To build trust and satisfaction, the company: 
+            <Layout image="/customer.png" title="Customer" desc="Customers are the backbone of any distribution company. To build trust and satisfaction, the company: 
             Offers Transparent Communication – Real-time tracking and responsive customer support keep customers informed.
             Adapts to Customer Needs – Flexible services, bulk orders, and customized delivery options improve customer experience.Seeks Feedback & Improvement – Regular customer surveys help improve service quality." classStyle="lg:bg-lime-700 lg:text-white"/>
 
-            <Layout image="public/driver.jpg" title="Drivers" desc="Drivers play a crucial role in ensuring smooth distribution. A strong relationship with them enhances efficiency: Proper Training & Support – Well-trained drivers understand safety, efficiency, and customer service. Fair Compensation & Incentives – Bonuses for performance and fuel allowances keep drivers motivated.
+            <Layout image="/driver.jpg" title="Drivers" desc="Drivers play a crucial role in ensuring smooth distribution. A strong relationship with them enhances efficiency: Proper Training & Support – Well-trained drivers understand safety, efficiency, and customer service. Fair Compensation & Incentives – Bonuses for performance and fuel allowances keep drivers motivated.
             Advanced Route Planning – GPS tracking and optimized routes reduce stress and improve delivery times." box ={true} />
 
-            <Layout image="public/partners.png" title="Partners" desc="We rely on strong partnerships with suppliers, manufacturers, and retailers. Effective collaboration is key to a smooth supply chain:
+            <Layout image="/partners.png" title="Partners" desc="We rely on strong partnerships with suppliers, manufacturers, and retailers. Effective collaboration is key to a smooth supply chain:
              Reliable Supply Chain Management – Coordinating with partners ensures steady inventory flow.
                 Mutual Growth Opportunities – Long-term partnerships help businesses expand and reach new markets."  classStyle="lg:bg-lime-700 lg:text-white" />
 
